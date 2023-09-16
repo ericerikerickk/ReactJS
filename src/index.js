15611d92c009926b1e3c9c9302aae5924bbe81f8
Eric Poblete <113760743+ericerikerickk@users.noreply.github.com>
2023-09-16 16:51:10 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./app";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap.css";
+//const element = <h1>Hello World123!</h1>;
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
+    , document.getElementById('root'));
---
+++ src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class register extends Component {
+    render() {
+        return (
+            <div>
+                Register Page
+            </div>
+        );
+    }
+}
+
+export default register;
